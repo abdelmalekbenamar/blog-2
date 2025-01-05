f4d25a82791555d6d72fbdbfa601834537e8ec43
abdelmalekbenamar <abdelmalek.benamar1990@gmail.com>
2025-01-05 22:01:58 +0100

    js pour le menu principal et responsive et creation de page acceuil

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+<body class="bg-[burlywood]">
+
+    <?php include_once("./php/menu.php") ?>
+
+    <main>
+        <div class="articleCard">
+            <div class="containerArticleImage">
+                <img class="articleImage" src="https://flourist.com/cdn/shop/articles/IMG_7623_VSCO.jpg" alt="">
+            </div>
+            <h2 class="articleTitle"><a href="">Le titre de l'article</a></h2>
+            <p class="articleText">This simple salad can be dressed up or down depending on what you have in your fridge. We like it as is, with or without...</p>
+        </div>
+
+        <div class="articleCard">
+            <div class="containerArticleImage">
+                <img class="articleImage" src="https://flourist.com/cdn/shop/articles/IMG_7623_VSCO.jpg" alt="">
+            </div>
+            <h2 class="articleTitle"><a href="">Le titre de l'article</a></h2>
+            <p class="articleText">This simple salad can be dressed up or down depending on what you have in your fridge. We like it as is, with or without...</p>
+        </div>
+
+        <div class="articleCard">
+            <div class="containerArticleImage">
+                <img class="articleImage" src="https://flourist.com/cdn/shop/articles/IMG_7623_VSCO.jpg" alt="">
+            </div>
+            <h2 class="articleTitle"><a href="">Le titre de l'article</a></h2>
+            <p class="articleText">This simple salad can be dressed up or down depending on what you have in your fridge. We like it as is, with or without...</p>
+        </div>
+
+        <div class="articleCard">
+            <div class="containerArticleImage">
+                <img class="articleImage" src="https://flourist.com/cdn/shop/articles/IMG_7623_VSCO.jpg" alt="">
+            </div>
+            <h2 class="articleTitle"><a href="">Le titre de l'article</a></h2>
+            <p class="articleText">This simple salad can be dressed up or down depending on what you have in your fridge. We like it as is, with or without...</p>
+        </div>
+
+        <div class="articleCard">
+            <div class="containerArticleImage">
+                <img class="articleImage" src="https://flourist.com/cdn/shop/articles/IMG_7623_VSCO.jpg" alt="">
+            </div>
+            <h2 class="articleTitle"><a href="">Le titre de l'article</a></h2>
+            <p class="articleText">This simple salad can be dressed up or down depending on what you have in your fridge. We like it as is, with or without...</p>
+        </div>
+    </main>
+    
+
+
+    <script src="./assets/js/indexScript.js"></script>
+</body>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,46 @@
+
+
+<div class="menuResponsive absolute left-[-250px] duration-[0.3s] transition-[left] ease-[ease-in] z-[100]">
+<nav class="nav w-[250px] h-screen bg-[bisque]">
+    <div class="fermer text-right cursor-pointer p-2.5 border-b-[black] border-b border-solid">X</div>
+    <ul>
+        <li class="li text-center p-2.5 border-b-[black] border-b border-solid"><a href="#">Accueille</a></li>
+        <li class="li text-center p-2.5 border-b-[black] border-b border-solid"><a href="#">Paramètres</a></li>
+        <li class="li text-center p-2.5 border-b-[black] border-b border-solid"><a href="#">Ajouter Article</a></li>
+        <li class="li text-center p-2.5 border-b-[black] border-b border-solid"><a href="#">Déconnexion</a></li>
+    </ul>
+
+</nav>
+</div>
+
+<div class="menuPrincipal flex justify-between items-center p-[10px]">
+<div class="menuPrincipalImages md:hidden">
+    <img class="MPHamburger w-[30px] cursor-pointer" src="./assets/images/hamburger-menu.png" alt="">
+</div>
+<img class="MPLogo h-20" src="./assets/images/logo.png" alt="">
+<nav class="navMP hidden md:block">
+    <ul class="listMenuPrincipal flex justify-between">
+        <li class="MPLi mx-5 my-0"><a href="#">Accueille</a></li>
+        <li class="MPLi mx-5 my-0"><a href="#">Paramètres</a></li>
+        <li class="MPLi mx-5 my-0"><a href="#">Ajouter Article</a></li>
+        <li class="MPLi mx-5 my-0"><a href="#">Déconnexion</a></li>
+    </ul>
+</nav>
+<div>
+    <div class="menuInfoSearch flex justify-between items-center w-[110px]">
+        <div class="user">username</div>
+        <img class="MPSearch h-[30px] cursor-pointer" src="./assets/images/search.png" alt="">
+    </div>
+</div>
+
+</div>
+
+<form class="chercherArticleForm w-screen absolute bg-[burlywood] top-[-105px] duration-[0.3s] transition-[top] ease-[ease-in]" action="" method="POST">
+<input name="articleARecherhcer" class="inputChercherArticle w-full h-[50px] bg-[burlywood] rounded-[10px] border-b-[bisque] border-b border-solid" type="text" placeholder="Rechercher...">
+<div class="rechercherFormButtons w-fit mx-auto my-0">
+    <button class="submitSearchButton border m-2.5 p-[5px] rounded-[5px] border-solid border-[black]" type="submit">Chercher</button>
+    <button class="cancelSearchButton border m-2.5 p-[5px] rounded-[5px] border-solid border-[black]">Annuler</button>
+</div>
+
+</form>
+
