85661a2c0529f7b1bc98a3052a7ad767e7621037
abdelmalekbenamar <abdelmalek.benamar1990@gmail.com>
2025-01-08 12:29:06 +0100

    front end pour la page article et commentaire du dashboard

--- parametres.php
+++ parametres.php
@@ -10,23 +10,40 @@
 <body class="bg-[burlywood]">
     <?php include_once("./php/menu.php") ?>
 
-    <main>
-        <div class="menuDashboard">
-            <div class="articles">
-                
-            </div>
-            <div class="commentaires">
-
-            </div>
-            <div class="utilisateur">
-
-            </div>
+    <main class="flex justify-between">
+        <div class="menuDashboard w-[80px] md:w-[100px] h-[500px] bg-[linear-gradient(-90deg,#ff7f50,burlywood)] text-center flex flex-col justify-around text-xs rounded-[10px] md:mt-[50px]">
+            <?php include_once("./php/dashboard.php"); ?>
         </div>
 
+    
+        <div class="partieDroite flex justify-around w-[calc(100vw_-_80px)] pt-[100px] flex-wrap">
 
-        <div class="partieDroite">
+            <div class="userCard h-[150px] w-[300px] bg-[linear-gradient(45deg,burlywood,chocolate)] p-5 rounded-[10px]">
+                <h2>Users</h2>
+                <div class="userCardInfo mt-[20px] flex justify-around items-center">
+                    <img class="cardImage w-[60px]" src="./assets/images/users.png" alt="">
+                    <span class="text-[28px]">5</span>
+                </div>
+            </div>
+
+            <div class="userCard h-[150px] w-[300px] bg-[linear-gradient(45deg,burlywood,chocolate)] p-5 rounded-[10px]">
+                <h2>Articles</h2>
+                <div class="userCardInfo mt-[20px] flex justify-around items-center">
+                    <img class="cardImage w-[60px]" src="./assets/images/articles.png" alt="">
+                    <span class="text-[28px]">45</span>
+                </div>
+            </div>
+
+            <div class="userCard h-[150px] w-[300px] bg-[linear-gradient(45deg,burlywood,chocolate)] p-5 rounded-[10px]">
+                <h2>Catégories</h2>
+                <div class="userCardInfo mt-[20px] flex justify-around items-center">
+                    <img class="cardImage w-[60px]" src="./assets/images/category.png" alt="">
+                    <span class="text-[28px]">8</span>
+                </div>
+            </div>
 
         </div>
+    
     </main>
 
 
---
+++ php/dashboard.php
@@ -0,0 +1,16 @@
+            <div class="articles">
+                <img class="w-[25px] block mx-auto my-0" src="./assets/images/articles.png" alt="">
+                <p><a href="./parametreArticles.php">Articles</a></p>
+            </div>
+            <div class="commentaires">
+                <img class="w-[25px] block mx-auto my-0" src="./assets/images/comments.png" alt="">
+                <p><a href="./parametreCommentaires.php">Commentaires</a></p>
+            </div>
+            <div class="utilisateurs">
+                <img class="w-[25px] block mx-auto my-0" src="./assets/images/users.png" alt="">
+                <p><a href="#">Utilisateurs</a></p>
+            </div>
+            <div class="categories">
+                <img class="w-[25px] block mx-auto my-0" src="./assets/images/category.png" alt="">
+                <p><a href="#">Catégories</a></p>
+            </div>
